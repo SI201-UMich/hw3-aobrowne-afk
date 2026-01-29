d946770750c47630805433d672b244586c34593f
Anna Browne <annabrowne@Annas-MacBook-Pro-17.local>
2026-01-28 19:22:01 -0500

    distribute_session func

--- HW3.py
+++ HW3.py
@@ -36,7 +36,6 @@ class CouponDispenser:
             coupon_cards (list[str]): list of possible coupons users can receive.
         """
         # TODO: Implement per instructions
-        pass
 
     def __str__(self):
         if len(self.coupon_cards) == 0:
@@ -52,17 +51,16 @@ class CouponDispenser:
             str
         """
         # TODO: Implement per instructions
-        pass
 
     def issue_coupon(self, name):
        if len(self.coupon_cards) == 0:
             return "The box is empty."
-       for name in user_input.split(","):
-            if name in self.customer_roster:
-                return f"That name already has a coupon: {self.coupon_cards[self.issued_indices[name.index()]]}"
+       for person in name.split(","):
+            if person in self.customer_roster:
+                return f"That name already has a coupon: {self.coupon_cards[self.issued_indices[person.index()]]}"
             else:
                 coupon_index = random(0, len(self.coupon_cards))
-                self.customer_roster.append(name)
+                self.customer_roster.append(person)
                 self.issued_indices.append(self.coupon_cards[coupon_index])
         """
         Assign name with a random coupon. If name is already assigned a coupon, return it.
@@ -78,19 +76,24 @@ class CouponDispenser:
             str: message as described above
         """
         # TODO: Implement per instructions
-        pass
 
     def distribute_session(self):
         round_number = 1
 
         user_input = input(f"Round {round_number} - Enter a name (or a comma-separated list), or type 'show' or 'exit': ")
         self.user_input = user_input
-        for name in self.user_input.split(","):
-            self.customer_roster.append(name)
-        for customer in self.customer_roster:
-            if customer == "exit":
-                print("Goodbye!")
-                break
+        if self.user_input == "exit":
+            print("Goodbye!")
+        elif self.user_input == "show":
+            for i in range(len(self.customer_roster)):
+                print(f"{self.customer_roster[i]}: {self.coupon_cards[self.issued_indices[i]]}")
+        else:
+            list_input = user_input.split(",")
+            for name in list_input:
+                stripped_name = name.strip()
+                if stripped_name == "":
+                    continue
+                print(issue_coupon(stripped_name))
         """
         Run the "coupon dispenser" session.
 
@@ -106,7 +109,6 @@ class CouponDispenser:
         Reminder: Use lists only (no dictionaries).
         """
         # TODO: Implement per instructions 
-        pass
 
     def tally_distribution(self):
         if len(self.issued_indices) == 0:
